3d0c0ef9bd70bb95a220c0158cac87fc142073d2
isaev-n <isaev.proger@gmail.com>
2026-06-17 12:08:02 +0300

    1 try

---
+++ src/test/java/SearchTests.java
@@ -0,0 +1,11 @@
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SearchTests {
+    @Test
+    void successfulSearchTest() {
+        open("https://www.google.com/");
+    }
+}
